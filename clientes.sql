566e1cd3afd5ca52ef6447d13bd224545a3b70c6
robert-virtual <robertocastill63@gmail.com>
2021-11-24 15:28:55 -0600

    schema modificado

--- clientes.sql
+++ clientes.sql
@@ -6,3 +6,19 @@ VALUES ('0703200101235','Roberto Castillo','88137603','Danli El Paraiso','robert
 ('0703198501232','Daysi Castellanos','88137601','La Ceiba Atlantidad','daysi@gmail.com'),
 ('0703199001233','Ivis Valle','88137600','Residencial El Dorado Tegucigalpa, Francisco Morazan','ivis@gmail.com')
 
+
+update Clientes set Registrado = '2021-10-24' 
+where IdCliente  = 1
+
+update Clientes set Registrado = '2020-10-24' 
+where IdCliente  = 2 
+
+update Clientes set Registrado = '2020-1-24' 
+where IdCliente  = 3 
+
+update Clientes set Registrado = '2020-5-24' 
+where IdCliente  = 4 
+
+
+
+select * from Clientes
