69da9329883cfeda88bd707550d973e7f68c6766
robert-virtual <robertocastill63@gmail.com>
2021-11-24 21:25:00 -0600

    crear usuario

--- crearusuario-smple.sql
+++ crearusuario-smple.sql
@@ -8,4 +8,8 @@ CREATE LOGIN robert12
 use ebaydb
 CREATE USER robert12 FOR LOGIN robert12;  
 GO
+-- agregar permisos
 alter role db_datareader add member robert12
+--- db_datareader
+--- db_datawriter
+--- db_ddladmin : create,alter,drop
